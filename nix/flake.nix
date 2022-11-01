b89722fee217f3603e268cdacea3ec2713f1a05a
Dylan Meskis <dmeskis@gmail.com>
2022-11-01 13:09:51 -0600

    more testing

--- nix/flake.nix
+++ nix/flake.nix
@@ -26,7 +26,50 @@
         specialArgs = { inherit nixpkgs; };
       },
     in {
+      homeConfigurations.dylanmeskis = home-manager.lib.homeManagerConfiguration {
+        # inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
       defaultPackage.aarch64-darwin = darwinARMSystem.system;
     };
 }
+
+# {
+#   description = "dylan's config";
+
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#     home-manager = {
+#       url = "github:nix-community/home-manager";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#     darwin = {
+#       url = "github:lnl7/nix-darwin/master";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+
+#   outputs = { darwin, nixpkgs, home-manager, ... }:
+#     # let
+#     #   system = "aarch64-darwin";
+#     #   pkgs = nixpkgs.legacyPackages.${system};
+#     # in 
+#     {
+#       darwinConfigurations."Dylans-MBP.home" = darwin.lib.darwinSystem {
+#         # inherit pkgs;
+#         # system = system;
+#         system = "aarch64-darwin";
+#         modules = [ ./darwin.nix ];
+#       };
+#       homeConfigurations.dylanmeskis = home-manager.lib.homeManagerConfiguration {
+#         # inherit pkgs;
+#         modules = [
+#           ./home.nix
+#         ];
+#       };
+#     };
+# }
+
 
