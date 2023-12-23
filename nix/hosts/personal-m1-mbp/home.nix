a1d88e3bd69b272bad2c6808d616d9944f263e2a
Dylan Meskis <dmeskis@gmail.com>
2023-12-23 14:21:53 -0700

    asdf

--- nix/hosts/personal-m1-mbp/home.nix
+++ nix/hosts/personal-m1-mbp/home.nix
@@ -3,4 +3,17 @@
   imports = [
     ../common.nix
   ];
+
+  programs = {
+    zsh = {
+      # shellAliases = import ./homebotAliases.nix;
+      # initExtraBeforeCompInit = builtins.readFile ./pre-compinit.zsh;
+      # initExtra = builtins.readFile ./post-compinit.zsh;
+
+      profileExtra = ''
+        # Set PATH, MANPATH, etc., for Homebrew.
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      '';
+    };
+  };
 }
